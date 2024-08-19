da68d2f1f2429a1f2f26348ec8e5b97aa9534736
Thiago Valmir Cardoso <thiagovalmircardoso@gmail.com>
2024-08-19 15:18:32 -0300

    fix: made some visual changes

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class MainColor {
-  static Color primaryColor = Color(0xFFff4b4b);
-  static Color secondaryColor = Color(0xFFffca27);
-  static Color accentColor = Color(0xFF4169e8);
+  static Color primaryColor = Color(0xff000000);
+  static Color secondaryColor = Color(0xff717171);
+  static Color accentColor = Color(0xffffea00);
 }
